226f29611fcca882d129a324f665a6bba800ebd1
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-04-21 18:23:32 -0300

    Inicio terceiro dia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-import { useEffect } from 'react';
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
-export default function Home(props) {
-  console.log(props.episodes)
+interface Episodes {
+  id: string,
+  title: string,
+  members: string;
+  published_at: string;
+}
+
+interface HomeProps {
+  episodes: Episodes[];
+}
+
+export default function Home(props: HomeProps) {
+
+
+
   return (
     <div>
       <h1>Index</h1>
@@ -10,9 +24,15 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
 
   return {
     props: {
