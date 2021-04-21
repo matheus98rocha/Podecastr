56cf6838cace9f42ca2008a5e18305325da14757
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-04-20 21:57:28 -0300

    Segundo dia NLW

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import { useEffect } from 'react';
 
-export default function Home() {
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-      .then(reponse => reponse.json())
-      .then(data => console.log(data))
-  }, [])
+export default function Home(props) {
+  console.log(props.episodes)
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
